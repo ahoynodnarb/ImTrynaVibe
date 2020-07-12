374d33547a953b0a8a462897b3862a51ad51df52
PopsicleTreehouse <54643196+PopsicleTreehouse@users.noreply.github.com>
2020-07-12 08:05:34 -0700

    Updated Prefs

--- imtrynavibeprefs/ITVPRootListController.m
+++ imtrynavibeprefs/ITVPRootListController.m
@@ -1,4 +1,5 @@
 #include "ITVPRootListController.h"
+#include <spawn.h>
 
 @implementation ITVPRootListController
 
@@ -10,6 +11,19 @@
 	return _specifiers;
 }
 -(void)openGithub {
-	[[UIApplication sharedApplication]openURL:[NSURL URLWithString:@"http://github.com/PopsicleTreehouse/ImTrynaVibe"] options:@{} completionHandler:nil];
+	[[UIApplication sharedApplication]openURL:[NSURL URLWithString:@"https://github.com/PopsicleTreehouse/ImTrynaVibe"] options:@{} completionHandler:nil];
+}
+-(void)openTwitter {
+	[[UIApplication sharedApplication]openURL:[NSURL URLWithString:@"https://twitter.com/ahoynodnarb"] options:@{} completionHandler:nil];
+}
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    UIBarButtonItem *applyButton = [[UIBarButtonItem alloc] initWithTitle:@"Apply" style:UIBarButtonItemStylePlain target:self action:@selector(applyChanges)];
+    self.navigationItem.rightBarButtonItem = applyButton;
+}
+-(void)applyChanges {
+	pid_t pid;
+	const char *argv[] = {"killall", "backboardd", NULL};
+	posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char* const*)argv, NULL);
 }
 @end
